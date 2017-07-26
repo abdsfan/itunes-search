52d96bbfca1589f5df3243ba2570bc30e02f4016
Abdullah Yousfan <ayousfan@pdsit.net>
2017-07-26 16:13:27 -0500

    added getAlbums and displayAlbums function

--- itunes-search.js
+++ itunes-search.js
@@ -10,27 +10,56 @@ function searchItunes(){
 	else{
 		refinedSearch = userSearchRequest;
 	}
-	fetch('https://itunes.apple.com/search?term=' + refinedSearch)
+	fetch('https://itunes.apple.com/search?term=' + refinedSearch + "&limit=200")
 		.then((resp)=> resp.json())
 		.then(function(data) {
 			console.log(data);
-			var onlySongs = filterForSongs(data);
-			filterByArtist(onlySongs);
+			var bySongs = filterForSongs(data);
+			getAlbums(bySongs);
 	});
 	
 }
 
 function filterForSongs(data) {
-	var onlySongs = data.results.filter(function(element){
+	var bySongs = data.results.filter(function(element){
 		if (element.kind === "song") {
 			return true;
 		} else {
 			return false;
 		}
 	});
-	return onlySongs;
+	return bySongs;
 }
 
+function getAlbums(bySongs){
+    var albumList = [];
+    var songsSortedByAlbum = [];
+    var album;
+    for (var i = 0; i < bySongs.length; i++) {
+        if(!(albumList.includes(bySongs[i].collectionName))){
+            albumList.push(bySongs[i].collectionName);
+        }
+    }
+    for (var i = 0; i < albumList.length; i++) {
+        album = bySongs.filter(function(element){
+            if(element.collectionName === albumList[i]){
+                return true;
+            }
+            else{
+                return false;
+            }
+    });
+    songsSortedByAlbum.push(album);
+}
+
+function displayAlbums() {
+	for (var albumName in songsSortedByAlbum) {
+	   var newElement = document.createElement('div');
+	   newElement.id = songsSortedByAlbum[albumName]; newElement.className = "album";
+	   newElement.innerHTML = songsSortedByAlbum[albumName][0].collectionName;
+	   document.body.appendChild(newElement);
+	}
+}
 
 
 function refineSearch(searchInput){
