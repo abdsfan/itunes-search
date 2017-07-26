974969c8e4df77408abdde5b23d9e6e39f955cee
lars-alfonse <lars.alfonse@gmail.com>
2017-07-26 11:44:14 -0500

    function to get search data and print to console

--- itunes-search.js
+++ itunes-search.js
@@ -1,3 +1,32 @@
 "use strict"
-//this is a test
-//please check this file delete the comments and send back
+function searchItunes(){
+	var userSearchRequest;
+	userSearchRequest = document.getElementById('searchBox').value;
+	var refinedSearch;
+	if(checkForSpaces(userSearchRequest)){
+	refinedSearch = refineSearch(userSearchRequest);
+	}
+	else{
+		refinedSearch = userSearchRequest;
+	}
+	fetch('https://itunes.apple.com/search?term=' + refinedSearch)
+		.then((resp)=> resp.json())
+		.then(function(data) {
+			console.log(data);
+	});
+}
+function refineSearch(searchInput){
+	var seperatedSearch;
+	var rejoinedSearch;
+	seperatedSearch = searchInput.split(" ");
+	rejoinedSearch = seperatedSearch.join("+");
+	return rejoinedSearch;
+}
+function checkForSpaces(userSearchRequest){
+	if(userSearchRequest.search(" ")>=0){
+		return true;
+	}
+	else{
+		return false;
+	}
+}
