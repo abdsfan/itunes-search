7ae70e6de8a75ba652d7d40557e7c5924e61d1e7
lars-alfonse <lars.alfonse@gmail.com>
2017-07-27 07:14:05 -0500

    added scrollable divs, function to generated div per song, added css page

--- itunes-search.js
+++ itunes-search.js
@@ -12,7 +12,13 @@ function searchItunes(){
 	fetch('https://itunes.apple.com/search?term=' + refinedSearch)
 		.then((resp)=> resp.json())
 		.then(function(data) {
-			console.log(data);
+			console.log(data.results);
+			var artistnames
+			artistnames = data.results.map(function(element){
+				return element.artistName;
+			});
+			artistnames = artistnames.join("\n");
+			document.getElementbyId('resultBox').innerHTML = artistnames;
 	});
 }
 function refineSearch(searchInput){
@@ -28,5 +34,41 @@ function checkForSpaces(userSearchRequest){
 	}
 	else{
 		return false;
+	}
+}
+$(".resultBox").scroll(function(){
+  var sticky = $('.sticky'),
+      scroll = $('.resultBox').scrollTop();
+
+  if (scroll >= 100) sticky.addClass('fixed');
+  else sticky.removeClass('fixed');
+});
+function getAlbums(results){
+	var albumList = [];
+	var songsSortedByAlbum = [];
+	var album;
+	for (var i = 0; i < results.length; i++) {
+		if(!(albumList.includes(results[i].collectionName))){
+			albumList.push(results[i].collectionName);
+		}
+	}
+	for (var i = 0; i < albumList.length; i++) {
+		album = results.filter(function(element){
+			if(element.collectionName === albumList[i]){
+				return true;
+			}
+			else{
+				return false;
+			}
+	});
+	songsSortedByAlbum.push(album);
+}
+function generateUserDisplay(sortedSongs){
+var resultBox = document.getElementById("resultBox")
+for (var albumName in songsSortedByAlbum) {
+    var newElement = document.createElement('div');
+    newElement.id = songsSortedByAlbum[albumName]; newElement.className = "album";
+    newElement.innerHTML = songsSortedByAlbum[albumName][0].collectionName;
+    document.body.appendChild(newElement);
 	}
 }
